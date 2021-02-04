57112a9c4d5e808d6d853ee683164f67e612eb33
Leigh-Jean <ljxrustin@gmail.com>
2021-02-04 10:27:53 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { ChatEngine } from 'react-chat-engine'
+import './App.css'
+
+const App = () => {
+    return (
+        <ChatEngine 
+            height="100vh"
+            projetID="778c1273-798a-4725-9cf5-f19eb12ece0b"
+            userName="Leigh-Jean"
+            userSecret="ChatApp4121"
+        />
+    )
+}
+
+export default App
